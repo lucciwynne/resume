e1e4ee07966ac48efaf5a5a8d94b40f76e51d76f
paulacaixeta <vivaluccistudio@gmail.com>
2019-01-04 13:47:05 -0800

    Implemented theme toggle functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 // CSS import
 import '../sass/main.scss';
+
+// Toggle theme
+document.querySelector('.resume__theme--switch').addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+});
