f1f15ffd6de3542320b9d7924a19594e90e26f8a
paulacaixeta <vivaluccistudio@gmail.com>
2019-01-04 15:15:51 -0800

    Used localStorage to remember theme setting on refresh

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,10 @@ import '../sass/main.scss';
 
 // Toggle theme
 document.querySelector('.resume__theme--switch').addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
+    let darkThemeEnabled = document.body.classList.toggle('dark-theme');
+    localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
 });
+
+if(JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
+    document.body.classList.add('dark-theme');
+}
